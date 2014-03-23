25488514b7012a858eb34e2b044cf396dbe5d002
Somkiat <somkiat.p@gmail.com>
2014-03-24 00:14:50 +0700

    Refactoring  code to remove if and convert integer to string

--- fizzbuzz.erl
+++ fizzbuzz.erl
@@ -2,12 +2,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 compute(_number) ->
-  if
-  	_number == 2 -> 
-  		"2";
-  	true -> 
-  		"1"
-  end.
+  integer_to_list(_number).
  
 one_should_return_one_test() ->
     ?assertEqual("1", compute(1)).
