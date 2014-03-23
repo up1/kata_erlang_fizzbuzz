99ea57abc7e156d472c82fc0ef82e42918ce7332
Somkiat <somkiat.p@gmail.com>
2014-03-24 00:13:03 +0700

    Resolve case 2 with if statement

--- fizzbuzz.erl
+++ fizzbuzz.erl
@@ -1,8 +1,13 @@
 -module(fizzbuzz).
 -include_lib("eunit/include/eunit.hrl").
 
-compute(_Num) ->
-  "1".
+compute(_number) ->
+  if
+  	_number == 2 -> 
+  		"2";
+  	true -> 
+  		"1"
+  end.
  
 one_should_return_one_test() ->
     ?assertEqual("1", compute(1)).
